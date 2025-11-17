8b95409718817108328a56623964d25676fe5610
SaraBarrett <sara.c.mon@gmail.com>
2025-11-17 17:55:50 +0000

    vars

---
+++ js/exs.js
@@ -0,0 +1,7 @@
+//pp 17
+const message = "   TASTE THE RAINBOW!  ";
+
+let whisper = message.toLowerCase().trim();
+console.log(whisper);
+
+//pp19
---
+++ js/script.js
@@ -0,0 +1,10 @@
+//primeiro script de JS
+//alert(16.777777 + 4); 
+
+let myName = 'Sara';
+
+console.log('olá ' + myName);
+
+let myNumber = 16;
+myNumber++;
+console.log('o tamanho de '+myName+ ' e '+myName.length)
