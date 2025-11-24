e8ab5c2978cccb5c7c81bc058e5799393868ad4d
SaraBarrett <sara.c.mon@gmail.com>
2025-11-24 16:33:24 +0000

    exercício

--- js/exs.js
+++ js/exs.js
@@ -1,7 +1,23 @@
 //pp 17
-const message = "   TASTE THE RAINBOW!  ";
+// const message = "   TASTE THE RAINBOW!  ";
 
-let whisper = message.toLowerCase().trim();
-console.log(whisper);
+// let whisper = message.toLowerCase().trim();
+// console.log(whisper);
 
-//pp19
+// //pp19
+// const word = "skateboard";
+// let facialHair = word.slice(5).replace("o", "e")
+
+// console.log(facialHair)
+
+//pp24
+// let primeiroNome = prompt(`Qual o teu primeiro nome?`)
+// let segundoNome = prompt(`E qual é o seu sobrenome?`)
+// console.log(`Olá ${primeiroNome} ${segundoNome}!!`)
+
+//pp30
+let planets = ['Mercúrio', 'Vénus', 'Terra', 'Mart',
+'Jupiter', 'Saturno', 'Urano', 'Neptuno', 'Plutão'];
+planets[3]= 'Marte';
+planets.pop();
+planets.unshift('Novo Planeta')
--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-//primeiro script de JS
-//alert(16.777777 + 4); 
+// //primeiro script de JS
+// //alert(16.777777 + 4); 
 
-let myName = 'Sara';
+// let myName = 'Sara';
 
-console.log('olá ' + myName);
+// console.log('olá ' + myName);
 
-let myNumber = 16;
-myNumber++;
-console.log('o tamanho de '+myName+ ' e '+myName.length)
+// let myNumber = 16;
+// myNumber++;
+// console.log('o tamanho de '+myName+ ' é '+myName.length+ '!')
+
+// console.log(`o tamanho de ${myName} é ${myName.length}!`)
+
+// let weather = promp('Como está o tempo hoje?')
+// alert('o tempo está ' +weather);
+
+let alunos = ['João', 'Tiago', 'Gonçalo', 'Rui', 'Duarte']
+alunos[3] = 'Silvio';
+alunos.unshift('Rafael')
+console.log(alunos)
